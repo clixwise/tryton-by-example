d8270ec77debb381be4dff0568463e308f8841b9
Marc <47741477+mrmarcrottiers@users.noreply.github.com>
2021-05-05 14:38:29 +0200

    Organize

--- 5.8/Utilities/Tryton 5.8 - Doc 01.01 - Installation & administration.database.tryt01.status.ps1
+++ 5.8/Utilities/Tryton 5.8 - Doc 01.01 - Installation & administration.database.tryt01.status.ps1
@@ -5,6 +5,14 @@ Write-Host "---------"
 Write-Host "1. Status"
 Write-Host "---------"
 docker ps -a
+docker volume ls
+docker network ls
 Start-Sleep -Seconds 20 # Replace by detecting database is 'up'
 docker exec -tiu postgres tryt01-postgres psql -c '\l+'
+
+# Step 2 : done
+Write-Host "-------"
+Write-Host "2. Done"
+Write-Host "-------"
+
 Pause
