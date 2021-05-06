bd71c302ca60a0f49f574c383765a41dba26f8f7
Marc <47741477+mrmarcrottiers@users.noreply.github.com>
2021-05-06 14:05:35 +0200

    Organize

--- Utilities/Tryton 5.8 - Doc 00.01 - Installation & administration.docker.tryt01.create.permanent.ps1
+++ Utilities/Tryton 5.8 - Doc 00.01 - Installation & administration.docker.tryt01.create.permanent.ps1
@@ -14,7 +14,7 @@ Write-Host "----------------------------"
 Write-Host "1. tryt01 database container"
 Write-Host "----------------------------"
 $TRYTON_VOL_DB = (Get-Location).tostring().replace("\","/").replace("C:/","c//") + "/"+"tryt01-database"
-# docker volume create tryt01-database
+# docker volume create tryt01-database - for future use
 docker run --name tryt01-postgres --env PGDATA=/var/lib/postgresql/data/pgdata --env POSTGRES_DB=tryt01 --env POSTGRES_PASSWORD=${POSTGRES_PASSWORD} --volume ${TRYTON_VOL_DB}:/var/lib/postgresql/data --network tryt01-network -p 5433:5432 --detach postgres
 Start-Sleep -Seconds 20 # Replace by detecting database is 'up'
 docker exec -tiu postgres tryt01-postgres psql -c '\l+'
@@ -31,7 +31,7 @@ Write-Host "-------------------------------------------------------------"
 Write-Host "3. tryt01 server containers : tryt01 & optionally tryt01-cron"
 Write-Host "-------------------------------------------------------------"
 $TRYTON_VOL_FI = (Get-Location).tostring().replace("\","/").replace("C:/","c//") + "/"+"tryt01-datafile"
-# docker volume create tryt01-datafile
+# docker volume create tryt01-datafile - for future use
 docker run --name tryt01 --env DB_HOSTNAME=tryt01-postgres --env DB_PASSWORD=${POSTGRES_PASSWORD} --volume ${TRYTON_VOL_FI}:/var/lib/trytond/db --network tryt01-network -p 8001:8000 --detach tryton/tryton
 docker run --name tryt01-cron --env DB_HOSTNAME=tryt01-postgres --env DB_PASSWORD=${POSTGRES_PASSWORD} --volume ${TRYTON_VOL_FI}:/var/lib/trytond/db --network tryt01-network --detach tryton/tryton trytond-cron -d tryt01 # 'tryton/tryton trytond-cron -d tryt01' : from 'tryton/tryton' run 'trytond-cron' against the 'tryt01' database
 
