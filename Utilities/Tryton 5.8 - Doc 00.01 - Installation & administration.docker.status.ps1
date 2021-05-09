099e79ba43e72ccb61368fe3557e31ce5031ebc5
Marc <47741477+clixwise@users.noreply.github.com>
2021-05-09 15:52:36 +0200

    Organize

--- Utilities/Tryton 5.8 - Doc 00.01 - Installation & administration.docker.status.ps1
+++
@@ -1,9 +0,0 @@
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser
-#
-Write-Host "-------------"
-Write-Host "Docker Status"
-Write-Host "-------------"
-docker ps -a
-docker volume ls
-docker network ls
-docker inspect -f '{{.Name}} - {{json .NetworkSettings.Networks}}' $(docker ps -aq)
--- Utilities/query.res_user.sql
+++
@@ -1,1 +0,0 @@
-select * from res_user;
